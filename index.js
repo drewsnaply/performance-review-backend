9967068f3943806be593e7e2059c9395f1eea1dc
Andrew Mintzell <amintzell@gmail.com>
2025-04-05 14:37:52 -0400

    Update backend port to 5000

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const {
 const { router: authRoutes } = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Comprehensive CORS Configuration
 const corsOptions = {
